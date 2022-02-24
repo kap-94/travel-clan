bc791aae868cec8f1da136055e8a5439dff74749
kap-94 <karamjp7@gmail.com>
2022-02-24 11:54:12 -0600

    Lazy loading added to category images, lazy-image selector created in base.scss

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,28 @@
+"use strict";
+
 const flashMessage = document.querySelector(".flash-message");
+
+// LAZY LOADING IMAGES
+const imageTargets = document.querySelector("img[data-srcset]");
+
+const loadImage = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", () => {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const observer = new IntersectionObserver(loadImage, {
+  root: null,
+  threshold: 0,
+});
 
 // FLASH MESSAGES FADE OUT
 window.addEventListener("load", () => {
